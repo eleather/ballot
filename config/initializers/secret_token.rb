87fe9bdb2cbfced745abe2592a14c5e33db941ac
Emily Leathers <peppermintdove@gmail.com>
2012-10-02 19:35:33 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ballot::Application.config.secret_token = 'c5ac684d2e75f962cc3520f5ce0bcecb94e9d0a7aeecb2471b1a16a715cac289bafd8f36f92c836e7d59d44f92ea7a63a959399565fa2a75660b09d90a726242'
